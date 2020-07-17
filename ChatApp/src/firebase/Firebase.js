411c8f176ee97fa0eaa4bd718f284726247aab71
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-07-17 12:45:59 +0530

    init firebase

---
+++ ChatApp/src/firebase/Firebase.js
@@ -0,0 +1,17 @@
+
+import firebase from 'firebase'
+require('firebase/auth')
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBvUbzxTwcWPdC4uH06r_sdFG-XCg-euzA",
+    authDomain: "chatapp-4ab81.firebaseapp.com",
+    databaseURL: "https://chatapp-4ab81.firebaseio.com",
+    projectId: "chatapp-4ab81",
+    storageBucket: "chatapp-4ab81.appspot.com",
+    messagingSenderId: "490618121486",
+    appId: "1:490618121486:web:fdf973be04dd948b04fb5f",
+    measurementId: "G-0FXDS5G67H"
+};
+
+let Firebase=firebase.initializeApp(firebaseConfig);
+export default Firebase;
--- ChatApp/src/screens/SignInScreen.js
+++ ChatApp/src/screens/SignInScreen.js
@@ -13,6 +13,9 @@ import Utility from '../util/Utility'
 
 import Strings from '../const/String'
 
+import firebase from '../firebase/Firebase'
+
+
 function SignInScreen() {
 
     const [email, setEmail] = useState('')
@@ -35,6 +38,43 @@ function SignInScreen() {
         return isValid
     }
 
+    //registration
+    registration = (email, password) => {
+        try {
+            setIsLoading(true)
+            firebase.auth().signInWithEmailAndPassword(email, password)
+                .then(user => {
+                    isLoading(false)
+                    Alert.alert("Logged In")
+
+                }).catch(error => {
+                    firebase.auth().createUserWithEmailAndPassword(email, password)
+                        .then(user => {
+                            setIsLoading(false)
+                            Alert.alert('Create New User')
+                        }).catch(error => {
+                            setIsLoading(false)
+                            console.log(error)
+                            Alert.alert(error)
+                        })
+                })
+        } catch (error) {
+            setIsLoading(false)
+            Alert.alert(error)
+        }
+    }
+
+    //auth eka client side
+    performAuth = () => {
+        const isValidEmail = validateEmailAddress()
+        const isValidPassword = validatePasswordField()
+        if (isValidEmail && isValidPassword) {
+            setEmailError('')
+            setPasswordError('')
+            registration(email, password)
+        }
+    }
+
 
 
 
@@ -43,28 +83,28 @@ function SignInScreen() {
             <KeyboardAvoidingView style={styles.container} behavior="height" enabled>
                 <View>
                     <SafeAreaView>
-                            <ImageBackground style={styles.logo} source={Images.chatApp}/>
-                            <EmailTextField
-                                term={email}
-                                error={emailError}
-                                placeHolder={String.EmailPlaceHolder}
-                                onTermChange={newEmail=>setEmail(newEmail)}
-                                onValidateEmailAddress={validateEmailAddress}
+                        <ImageBackground style={styles.logo} source={Images.chatApp} />
+                        <EmailTextField
+                            term={email}
+                            error={emailError}
+                            placeHolder={String.EmailPlaceHolder}
+                            onTermChange={newEmail => setEmail(newEmail)}
+                            onValidateEmailAddress={validateEmailAddress}
 
-                            />
-                            <PasswordTextField
-                                term={password}
-                                error={passwordError}
-                                placeHolder={Strings.PasswordPlaceHolder}
-                                onTermChange={newPassword=>setPassword(newPassword)}
-                                onValidatePasswordField={validatePasswordField}
+                        />
+                        <PasswordTextField
+                            term={password}
+                            error={passwordError}
+                            placeHolder={Strings.PasswordPlaceHolder}
+                            onTermChange={newPassword => setPassword(newPassword)}
+                            onValidatePasswordField={validatePasswordField}
 
-                            />
-                            <Button
-                                title={Strings.Join}
-                            
-                                
-                            />
+                        />
+                        <Button
+                            title={Strings.Join}
+
+
+                        />
 
                     </SafeAreaView>
                 </View>
@@ -80,16 +120,16 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: Color.smoke,
-        
+
     },
     logo: {
 
         alignSelf: 'center',
         margin: 0.04 * Constants.screenHeight,
-        width: 150, 
+        width: 150,
         height: 150,
-        
-        
+
+
 
 
     }
