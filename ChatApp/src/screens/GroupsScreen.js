4cc9cde4e83c13e4a5af1cb86d0071d0831ac9b0
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-07-18 11:31:34 +0530

    add group item component

--- ChatApp/src/screens/GroupsScreen.js
+++ ChatApp/src/screens/GroupsScreen.js
@@ -1,8 +1,12 @@
 import React,{useLayoutEffect,useState,useEffect} from 'react'
-import {StyleSheet,View,Text} from 'react-native'
+import {StyleSheet,View,Text, FlatList, TouchableOpacity, Alert } from 'react-native'
 import ButtonWithBackground from '../components/ButtonWithBackground'
 import Images from '../const/Images'
+import GroupItem from '../components/GroupsItems'
+import firebase, { firestore } from '../firebase/Firebase'
 function GroupsScreen({navigation}) {
+    const [groups, setGroups] = useState([])
+
 
     useLayoutEffect(()=>{
         navigation.setOptions({
@@ -24,11 +28,54 @@ function GroupsScreen({navigation}) {
             )
         })
     })
+
+    useEffect(() => {
+        getChats()
+    }, [])
+
+    function getChats() {
+        const db = firestore
+        var groupArray = []
+
+        db.collection("groups")
+            .onSnapshot(function (snapshot) {
+                snapshot.docChanges().forEach(function (change) {
+                    if (change.type == "added") {
+                        console.log("New Group: ", change.doc.data())
+                        groupArray.push(change.doc.data())
+                    }
+                    if (change.type === "modified") {
+                        console.log("Modified Group: ", change.doc.data())
+                    }
+                    if (change.type === "removed") {
+                        console.log("Removed Group", change.doc.data())
+                    }
+
+                    setGroups(groupArray)
+                })
+            })
+
+    } 
     
     return (
+
         <View style={styles.container}>
-            <Text style={styles.text}> Gropus Screen </Text>
-            
+            <FlatList
+                data={groups}
+                keyExtractor={(item, index) => 'key' + index}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity onPress={() => {
+                            navigation.navigate('ChatScreen', {
+                                item
+                            })
+                        }}>
+                            <GroupItem item={item}></GroupItem>
+                        </TouchableOpacity>
+                    )
+                }}
+            >
+            </FlatList>
         </View>
     )
 }
