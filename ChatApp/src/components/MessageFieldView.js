32b24f860b4dad8d7a41515e8415693fc5315b70
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-07-18 20:40:48 +0530

    can access to third party person

---
+++ ChatApp/src/components/MessageFieldView.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { TextInput, Text, StyleSheet, View,Button } from 'react-native'
+import Color from '../util/Colors'
+import Constants from '../const/Constants'
+import Strings from '../const/String'
+
+
+const MessageFieldView = ({ term, placeHolder, onTermChange, onValidateTextField, error, onSubmit, isJoined }) => {
+    return (
+        <View style={styles.containerView}>
+            <View style={styles.fieldView}>
+                <TextInput
+                    autoCorrect={false}
+                    style={styles.textField}
+                    placeholder={placeHolder}
+                    value={term}
+                    onChangeText={onTermChange}
+                    onEndEditing={onValidateTextField}
+                />
+                <Button title = {Strings.Send} color = {Color.uaStudiosGreen} style={styles.Button} onPress = {onSubmit} />
+
+            </View>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    containerView: {
+        backgroundColor: Color.white,
+        width: Constants.screenWidth,
+        flex: 1,
+        justifyContent: 'space-between',
+    },
+    fieldView: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: Color.uaStudiosGreen
+
+    },
+    textField: {
+        fontSize: 14,
+        flex: 1,
+        marginRight: 0,
+        paddingLeft: 10,
+        width: '75%',
+        borderColor: Color.gray,
+        borderWidth: 1,
+        justifyContent: 'center',
+        backgroundColor: Color.smoke
+    },
+    Button: {
+        flex: 1,
+        alignSelf: 'center',
+        width: '25%',
+        height: '100%',
+        paddingLeft: 10,
+    }
+})
+
+export default MessageFieldView
